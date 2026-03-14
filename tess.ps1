b6ee5964e89e1cd7927ffa45c9e41ad087cd88dc
defloarina <defloarina@gmail.com>
2026-03-14 08:52:27 +0700

    Update tess.ps1

--- tess.ps1
+++ tess.ps1
@@ -1,9 +1,3 @@
-$code="& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AfrIepAb28AstoSGwRHRLgatZHw6o-dA9zDasIaDCp5FnUZF2MG99JCja117JXPNIIeL4Q" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME"
-$pin="123456"
-
-$path = "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe"
-
-& "$path" --code="$code" `
---redirect-url="https://remotedesktop.google.com/_/oauthredirect" `
---name=$env:COMPUTERNAME `
---pin="$pin"
+& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AfrIepDKyeLH99mabNvB2pFr6fwFCNOVJ0cKsEdTU-SsoBlfGtN-eV6JG4HJ8qc3jj_Jhg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
+123456
+123456
