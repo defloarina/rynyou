b1df95e3ffb300d7c679a69908a8f60f4ee519e8
defloarina <defloarina@gmail.com>
2026-03-14 08:58:08 +0700

    Update tess.ps1

--- tess.ps1
+++ tess.ps1
@@ -1,3 +1,1 @@
-& "${Env:PROGRAMFILES(X86)}\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" --code="4/0AfrIepDKyeLH99mabNvB2pFr6fwFCNOVJ0cKsEdTU-SsoBlfGtN-eV6JG4HJ8qc3jj_Jhg" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$Env:COMPUTERNAME
-123456
-123456
+
